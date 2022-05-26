652b89407061689c6f15deee6cdf6f35fde20dd4
Jude Boachie <boachiejude>
2022-05-26 18:04:56 +0000

    Updated algorithms

--- linkedlist.py
+++ linkedlist.py
@@ -1,31 +1,19 @@
-#linkedlist implementation in python 
 class Node:
-    def _init_(self,data):
-        self.data = data
-        self.next = None
+  def __init__(self, data=None,):
+    self.data = data
+    self.next = None
 
-#
-class linkedlist:
-    def _init_(self):
-        self.head = None   
+class LinkedList:
+  def __init__(self):
+    self.head = None
 
-    def listlist(self):
-        new_node =self.head
-        while new_node is not None:
-            print(new_node.data)
-            new_node = new_node.next
+  def show(self):
+    node = self.head
+    while node is not None:
+      print(node.data)
+      node = node.next
 
-list = linkedlist() 
-list.head = Node("Mon") 
-
-#create nodes
-e2 = Node("Tue")
-e3 = Node("Wed")
-
-#link first node to second node 
-list.head.next =e2
-
-#link second node to third node 
-e2.next = e3
-
-list.listlist()
+  def add(self, new,):
+    new_node = Node(new)
+    new_node.next = self.head
+    self.head = new_node
