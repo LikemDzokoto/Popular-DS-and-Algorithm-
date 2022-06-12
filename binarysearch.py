0ac1e977aacd58b7fff5a6364b9dc1443bd1ec81
Likem <likemdzokoto@gmail.com>
2022-06-12 06:53:46 +0000

    Update binarysearch.py

--- binarysearch.py
+++ binarysearch.py
@@ -1,36 +1,21 @@
-from typing import Any, List
-import unittest
+def binary_search(my_list, item):
+    low = 0
+    high = len(my_list) -1
+    
+    while low <= high:
+        mid = round((low + high)/2)
+        guess = my_list[mid]
+        
+        if guess == item:
+            return guess
+        if guess > item: #The guess was too high
+            high = mid -1
+        else: # The guess was too low
+            low = mid + 1
+    return None
+        
 
 
-def bin_search(arr: List[Any], target: Any) -> bool:
-    """Returns true if target is in arr"""
-    if len(arr) == 0:
-        return False
-    mid = len(arr) // 2
-    if target < arr[mid]:
-        return bin_search(arr[:mid], target)
-    elif target > arr[mid]:
-        return bin_search(arr[mid + 1:], target)
-    else:
-        return True
-
-
-class TestBinnarySearch(unittest.TestCase):
-    def test_empty_list(self):
-        self.assertFalse(bin_search([], 4))
-
-    def test_item_found(self):
-        self.assertTrue(bin_search([3, 4, 5, 6], 3))
-
-    def test_odd_array_length(self):
-        self.assertFalse(bin_search([0, 2, 4, 6, 8, 10, 12, 14, 16], 9))
-
-    def test_even_array_length(self):
-        self.assertTrue(bin_search([3, 4, 5, 6, 10, 20], 5))
-
-    def test_item_not_found(self):
-        self.assertFalse(bin_search([3, 4, 5, 6, 10, 20], 22))
-
-
-if __name__ == '__main__':
-    unittest.main()
+my_list = [1, 3, 5, 7, 9] 
+print(binary_search(my_list, 3)) # => 1
+print(binary_search(my_list, -1))# =>  None
